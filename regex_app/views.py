8b2f3d12ecac3eadaba1d489954cb4823dde2d32
mmka032 <156275523+mmka032@users.noreply.github.com>
2024-01-31 10:34:28 +0900

    if文を使って値がマッチしなかった場合エラーメッセージを表示させるようにする

--- regex_app/views.py
+++ regex_app/views.py
@@ -1,99 +1,118 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 
 import re
 
 def index(request):
-
-    
-    return render(request, 'regex_app/index.html')
+    # エラーメッセージを表示
+    context = {
+        'age': "",
+        'zip_code': "",
+        'tel': "",
+    }
+    return render(request, "regex_app/index.html", context)
 
 def confirm(request):
 
     # 全てのデータを取得
-    name=request.POST.get("name")
+    name = request.POST.get("name")
     age = request.POST.get("age")
-    zip_code=request.POST.get("zip_code")
-    tel=request.POST.get("tel")
+    zip_code = request.POST.get("zip_code")
+    tel = request.POST.get("tel")
 
     # 入力されたが値をチェック
-    checkname =re.sub(" ","",name)
-    checkage=re.match("[0-9]{1,3}",age)
-    checkzip_code=re.match("([0-9]{3}-?[0-9]{4})",zip_code)
-    checktel=re.match("([0-9]{3}-?[0-9]{4}-?[0-9]{4})",tel)
+    checkname = re.sub(" ", "", name)
+    checkage = re.match("[0-9]{1,3}", age)
+    checkzip_code = re.match("([0-9]{3}-?[0-9]{4})", zip_code)
+    checktel=re.match("([0-9]{3}-?[0-9]{4}-?[0-9]{4})" ,tel)
 
-    #以下 それぞれマッチした値だけを保存
+    #以下 全ての値がマッチしたらconfirm.htmlへ遷移
     if checkage and checkzip_code and checktel is not None:
+        
         context = {
-           "name":checkname,
+           "name": checkname,
            "age": checkage.group(),
-           "zip_code":checkzip_code.group(),
-           "tel":checktel.group(),
+           "zip_code": checkzip_code.group(),
+           "tel": checktel.group(),
         }
-        return render(request,"regex_app/confirm.html", context)  
-    
 
+        return render(request, "regex_app/confirm.html", context)  
+    
+    #以下 それぞれマッチした値だけを保存
     elif checkage and checkzip_code is not None:
-            tel=""
+            tel = ""
+
             context = {
-                "name":name,
-                "age":age,
-                "zip_code":zip_code,
+                "name": name,
+                "age": age,
+                "zip_code": zip_code,
             }
-            return render(request,"regex_app:index.html",context)
+
+            return render(request, "regex_app:index.html", context)
     
 
     elif  checkage and checktel is not None:
-            zip_code=""
-            context={
-                "name":name,
-                "age":age,
-                "tel":tel,
+            zip_code = ""
+
+            context = {
+                "name": name,
+                "age": age,
+                "tel": tel,
             }
-            return render(request,"regex_app/index.html",context)
+
+            return render(request, "regex_app/index.html", context)
     
 
     elif checkzip_code and checktel is not None:
-            age=""
-            context={
-                "name":name,
-                "zip_code":zip_code,
-                "tel":tel,
+            age = ""
+
+            context = {
+                "name": name,
+                "zip_code": zip_code,
+                "tel": tel,
             }
-            return render(request,"regex_app/index.html",context)
+
+            return render(request, "regex_app/index.html", context)
     
 
     elif  checkage is not None:
             zip_code = ""
-            tel=""
+            tel = ""
+
             context = {
-                "name":name,
-                "age":age,
+                "name": name,
+                "age": age,
             }
-            return render(request,"regex_app/index.html",context)
+
+            return render(request, "regex_app/index.html", context)
     
 
     elif checkzip_code is not None:
-            age=""
-            tel=""
+            age = ""
+            tel = ""
+
             context = {
-                "name":name,
-                "zip_code":zip_code
+                "name": name,
+                "zip_code": zip_code
             }
-            return render(request,"regex_app/index.html",context)
+
+            return render(request, "regex_app/index.html", context)
     
 
     elif  checktel is not None:
-            age=""
-            zip_code=""
+            age = ""
+            zip_code = ""
+
             context = {
-                "name":name,
-                "tel":tel,
+                "name": name,
+                "tel": tel,
             }
-            return render(request,"regex_app/index.html",context)
+
+            return render(request, "regex_app/index.html", context)
     
     else:
-            context={
-                "name":name,
-            }
-            return render(request,"regex_app/index.html",context)
-    
+        
+        context={
+            "name": name,
+        }
+            
+        return render(request, "regex_app/index.html", context)
